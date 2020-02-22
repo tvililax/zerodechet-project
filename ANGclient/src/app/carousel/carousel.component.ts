f5c2f55ad25b5538398fb000b25ddfb0aa800c89
Thomas da Costa <thomas.dacosta@adventori.com>
2020-02-22 18:06:33 +0100

    Edit middle section and text content

--- ANGclient/src/app/carousel/carousel.component.ts
+++ ANGclient/src/app/carousel/carousel.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
 
