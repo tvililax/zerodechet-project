1835d8a291c4450211a1f43aa6773eda69fc22a0
Thomas da Costa <thomasf.dacosta@gmail.com>
2020-02-26 13:31:08 +0100

    Edit form and create Form component

--- ANGclient/src/app/app.module.ts
+++ ANGclient/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselComponent
+    CarouselComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
